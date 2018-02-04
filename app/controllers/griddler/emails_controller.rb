8fb7838f6b99e642d137dce56ad97e4a8c372d17
Muhammad <muhammad@xanda.net>
2018-02-04 08:42:17 +0000

    fix error

--- app/controllers/griddler/emails_controller.rb
+++ app/controllers/griddler/emails_controller.rb
@@ -1,4 +1,5 @@
 class Griddler::EmailsController < ActionController::Base
+  protect_from_forgery with: :exception, prepend: true
   skip_before_action :verify_authenticity_token
   def create
     normalized_params.each do |p|
